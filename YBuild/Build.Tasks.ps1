5eb5fbe148fc47546452bcbc2c2fb31350d95cef
Manoj Mahalingam <manojlds@gmail.com>
2013-01-27 00:55:33 +0530

    Including only .ps1 files for commonfunctions and tasks

--- YBuild/Build.Tasks.ps1
+++ YBuild/Build.Tasks.ps1
@@ -1,6 +1,6 @@
 $script:dir = Split-Path -Parent $MyInvocation.MyCommand.Path
-gci $script:dir\..\CommonFunctions | ? { -not $_.FullName.EndsWith(".Tests.ps1") } | % { . $_.FullName }
-gci $script:dir\Tasks | ? { -not $_.FullName.EndsWith(".Tests.ps1") } | % { . $_.FullName }
+gci $script:dir\..\CommonFunctions -filter "*.ps1" | ? { -not $_.FullName.EndsWith(".Tests.ps1") } | % { . $_.FullName }
+gci $script:dir\Tasks -filter "*.ps1" | ? { -not $_.FullName.EndsWith(".Tests.ps1") } | % { . $_.FullName }
 
 if (Test-Path ("$rootDir\build.custom.tasks.ps1")) {
     "Importing custom build tasks..."
