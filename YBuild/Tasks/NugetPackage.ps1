a430410f536a941fea583243049c3e9b01c9a7a2
Manoj Mahalingam <manojlds@gmail.com>
2013-01-27 05:16:51 +0530

    Propogating build mode in nuget task

--- YBuild/Tasks/NugetPackage.ps1
+++ YBuild/Tasks/NugetPackage.ps1
@@ -1,10 +1,12 @@
 task NugetPackage {
-    $buildPath, $libPath = Get-Conventions buildPath, libPath
+    $buildPath, $libPath, $buildMode = Get-Conventions buildPath, libPath, buildMode
     $nuget = "$libPath\Nuget\Nuget.exe"
 
     $buildConfig["nugetPackageProjects"] | %{
         $project = Get-ChildItem "$rootDir\$_" -recurse
 
-        Exec { & $nuget pack $project -BasePath $buildPath -Version $buildVersion -OutputDirectory $buildPath -Properties Configuration=Release } "Nuget packaging failed"
+        Exec { 
+            & $nuget pack $project -BasePath $buildPath -Version $buildVersion -OutputDirectory $buildPath -Properties Configuration=$buildMode 
+        } "Nuget packaging failed"
     }
 }
