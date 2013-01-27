85b8da2f5550f0fd82e7edd868de3f5786532f92
Manoj Mahalingam <manojlds@gmail.com>
2013-01-27 23:55:48 +0530

    Using nuspecs instead of csproj for package

--- YBuild/Tasks/NugetPackage.ps1
+++ YBuild/Tasks/NugetPackage.ps1
@@ -2,7 +2,7 @@ task NugetPackage {
     $buildPath, $libPath, $buildMode = Get-Conventions buildPath, libPath, buildMode
     $nuget = "$libPath\Nuget\Nuget.exe"
 
-    $buildConfig["nugetPackageProjects"] | %{
+    $buildConfig["nugetSpecs"] | %{
         $project = Get-ChildItem "$rootDir\$_" -recurse
 
         Exec { 
