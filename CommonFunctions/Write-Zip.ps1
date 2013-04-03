9379aa8f2a2cba8335652236c5bf812d536d7b30
Manoj Mahalingam <manojlds@gmail.com>
2013-04-03 23:40:36 +0530

    Removing the iex!!

--- CommonFunctions/Write-Zip.ps1
+++ CommonFunctions/Write-Zip.ps1
@@ -2,9 +2,7 @@ function Write-Zip($source, $destination, $packageName){
     $libPath = Get-Conventions libPath
     $7z = "$libPath\7z\7za.exe"
     "Zipping $source to $destination\$packageName"
-    $zipCommand = "$7z a $(join-path $destination $packageName) $source"
-    
-    iex $zipCommand | out-null
+    & $7z a $(join-path $destination $packageName) $source | Out-Null
 }
 
 function Expand-Zip($source, $destination) {
