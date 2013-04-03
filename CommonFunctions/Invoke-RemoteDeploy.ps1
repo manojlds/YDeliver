8219891a09bdddbb440476fe1be192c41b5fc34a
Manoj Mahalingam <manojlds@gmail.com>
2013-04-03 14:15:59 +0530

    Closing the session in finally

--- CommonFunctions/Invoke-RemoteDeploy.ps1
+++ CommonFunctions/Invoke-RemoteDeploy.ps1
@@ -6,52 +6,55 @@ function Invoke-RemoteDeploy($server, $roleConfig, $version) {
 
     $session = New-PSSession -ComputerName $server
 
-    $modulePath = Install-YDeliver $remoteYDeliverPath $session
-    $installConfig = Get-Configuration $rootDir install
-    $workingDir = Join-Path $modulePath "temp"
+    try{
 
-    $installConfigConventions = $installConfig.conventions
-    if($installConfigConventions){
-        $installConfigConventions["artifactsDir"] = $workingDir
-    } else {
-        $installConfig.conventions = @{ "artifactsDir" = $workingDir; }
-    }
+        $modulePath = Install-YDeliver $remoteYDeliverPath $session
+        $installConfig = Get-Configuration $rootDir install
+        $workingDir = Join-Path $modulePath "temp"
 
-    $importedScripts = Import-Scripts @("$yDir\CommonFunctions\Get-Artifacts.ps1", "$yDir\CommonFunctions\Get-WebContent.ps1")
-
-    $deployConfig = @{
-        "modulePath" = $modulePath;
-        "workingDir" = $workingDir;
-        "installConfig" = $installConfig;
-        "roleConfig" = $roleConfig;
-        "version" = $version;
-        "remoteYDeliverPath" = $remoteYDeliverPath;
-        "importedScripts" = $importedScripts;
-    };
-   
-    "Using YDeliver from $modulePath"
-
-    Invoke-Command -Session $session -Command {
-        param($deployConfig)
-
-        $importedScripts = $ExecutionContext.InvokeCommand.NewScriptBlock($deployConfig["importedScripts"])
-        . $importedScripts
-        
-        if(Test-Path $deployConfig["workingDir"] -PathType Container){
-            rm $deployConfig["workingDir"] -force
+        $installConfigConventions = $installConfig.conventions
+        if($installConfigConventions){
+            $installConfigConventions["artifactsDir"] = $workingDir
+        } else {
+            $installConfig.conventions = @{ "artifactsDir" = $workingDir; }
         }
-        mkdir $deployConfig["workingDir"] | Out-Null
-        Set-Location $deployConfig["workingDir"]
 
-        Get-Artifacts $deployConfig["roleConfig"].artifacts $deployConfig["version"] $deployConfig["workingDir"]
-        
-        Import-Module "$($deployConfig["modulePath"])\YDeliver.psm1"
-        Invoke-YInstall -applications $deployConfig["roleConfig"].applications.keys -config $deployConfig["installConfig"]
-        
-        rm $deployConfig["workingDir"] -force
+        $importedScripts = Import-Scripts @("$yDir\CommonFunctions\Get-Artifacts.ps1", "$yDir\CommonFunctions\Get-WebContent.ps1")
 
-    } -ArgumentList @(, $deployConfig)
+        $deployConfig = @{
+            "modulePath" = $modulePath;
+            "workingDir" = $workingDir;
+            "installConfig" = $installConfig;
+            "roleConfig" = $roleConfig;
+            "version" = $version;
+            "remoteYDeliverPath" = $remoteYDeliverPath;
+            "importedScripts" = $importedScripts;
+        };
+       
+        "Using YDeliver from $modulePath"
 
-    Remove-PSSession $session
+        Invoke-Command -Session $session -Command {
+            param($deployConfig)
+
+            $importedScripts = $ExecutionContext.InvokeCommand.NewScriptBlock($deployConfig["importedScripts"])
+            . $importedScripts
+            
+            if(Test-Path $deployConfig["workingDir"] -PathType Container){
+                rm $deployConfig["workingDir"] -force
+            }
+            mkdir $deployConfig["workingDir"] | Out-Null
+            Set-Location $deployConfig["workingDir"]
+
+            Get-Artifacts $deployConfig["roleConfig"].artifacts $deployConfig["version"] $deployConfig["workingDir"]
+            
+            Import-Module "$($deployConfig["modulePath"])\YDeliver.psm1"
+            Invoke-YInstall -applications $deployConfig["roleConfig"].applications.keys -config $deployConfig["installConfig"]
+            
+            rm $deployConfig["workingDir"] -force
+
+        } -ArgumentList @(, $deployConfig)
+    } finally {
+        Remove-PSSession $session
+    }
 
 }
