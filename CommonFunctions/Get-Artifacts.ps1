322eafd42d732af76d7ee27ff7856719e45d494a
Manoj Mahalingam <manojlds@gmail.com>
2013-04-04 10:31:40 +0530

    Adding artifact support for Go

--- CommonFunctions/Get-Artifacts.ps1
+++ CommonFunctions/Get-Artifacts.ps1
@@ -1,15 +1,18 @@
 function Get-Artifacts($artifactsConfig, $version, $destination, $environmentConfig){
     $artifactsConfig | %{
         $artifact = $_.keys[0]
-        if($_[$artifact]."teamcity-build-id"){
-            Download-TeamCityArtifact $_[$artifact]."teamcity-build-id" $artifact $version "$destination\$artifact" $environmentConfig."teamcity-server"
+        if($_[$artifact]."teamcity"){
+            Download-TeamCityArtifact $_[$artifact]."teamcity" $artifact $version "$destination\$artifact" $environmentConfig."teamcity-server"
+        } elseif($_[$artifact]."go"){
+            Download-GoArtifact $_[$artifact]."go" $artifact $version "$destination\$artifact" $environmentConfig."go-server"
+
         } elseif($_[$artifact]."path"){
             Copy-Item (Join-Path $_[$artifact]."path" $artifact) -destination $destination
         }
     }
 }
 
-function Build-URL {
+function Build-TeamCityArtifactUrl {
     param($project, $artifact, $version, $server)
 
     $url = "$server/guestAuth/repository/download/$project/"
@@ -24,10 +27,15 @@ function Build-URL {
     return $url += "/$artifact"
 }
 
-function Download-TeamCityArtifact {
-    param($project, $artifact, $version, $dest, $server)
-    
-    $url = Build-URL -project $project -artifact $artifact -version $version -server $server
+function Download-TeamCityArtifact($teamCityConfig, $artifact, $version, $dest, $server){
+    $url = Build-TeamCityArtifactUrl -project $teamCityConfig["build-id"] -artifact $artifact -version $version -server $server
+    "Downloading artifact from $url"
+    Get-WebContent $url $dest -Force
+}
+
+function Download-GoArtifact($goConfig, $artifact, $version, $dest, $server) {
+    write-host -fore yellow $artifact
+    $url = "$server/go/files/{0}/$version/{1}/1/{2}/$artifact" -f @($goConfig["pipeline"], $goConfig["stage"], $goConfig["job"])
     "Downloading artifact from $url"
     Get-WebContent $url $dest -Force
 }
